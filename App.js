675480d3acd34cfc5f4aea1b008778b8338d0d30
Andrea Rosales <andrearosr@gmail.com>
2022-02-01 09:39:38 -0500

    Add input and button

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} />
+        <Button onPress={() => {}} title="Add" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +15,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 30,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+  },
+  input: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginRight: 20,
   },
 });
